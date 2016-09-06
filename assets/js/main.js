816674d9e5cacc03623e17256eb530b215987376
stanisger <gersigner@hotmail.com>
2016-09-06 11:31:45 -0500

    index full

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
+//WOW
+// new WOW().init();
+// animateMenu
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $(".clearHeader").addClass("shadowHeader");
+    } else {
+        $(".clearHeader").removeClass("shadowHeader");
+    }
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $(".clearNav").addClass("shadowNav");
+    } else {
+        $(".clearNav").removeClass("shadowNav");
+    }
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $(".clearOl").addClass("showOl");
+    } else {
+        $(".clearOl").removeClass("showOl");
+    }
+});
+
+
+
 //video HTML5
 function play() {
   var video = document.getElementById("video-parque-sur");
@@ -29,8 +60,42 @@ $('form#form-contact').on('submit', function(event) {
 
 
 $(document).ready(function(){
+      new WOW().init();
+  //PAGE SCROL
+  $('a.page-scroll').bind('click', function(event) {
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollTop: ($($anchor.attr('href')).offset().top -60)
+          }, 1350);
+          event.preventDefault();
+    });
+
+  //ACTIVE
+   var sections = $('section')
+      , nav = $('nav')
+      , nav_height = nav.outerHeight();
+ 
+    $(window).on('scroll', function () {
+      var cur_pos = $(this).scrollTop();
+ 
+      sections.each(function() {
+      var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        sections.removeClass('active');
+ 
+        $(this).addClass('active');
+        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      }
+      
+      });
+      });
+
   // TABS
   $('ul.tabs').tabs();
+
 
   // SLIDE
   $('.slider').slider({full_width: true});
